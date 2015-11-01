021339bdf69f54b1436962c0a6c61399d9b1a83f
hanwang92 <hanwang92@gmail.com>
2015-11-01 16:37:30 -0500

    bedroom demo complete
    version 1

---
+++ Assets/camera_rotate.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera_rotate : MonoBehaviour {
+
+	int animation_frame = 0;
+    public Fader fade;
+    float adjust = 0.0f;
+
+    void Start ()
+    {
+        fade = FindObjectOfType(typeof(Fader)) as Fader;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject floor;
+        floor = GameObject.Find("FLOOR");
+        Vector3 newpos = floor.transform.forward;
+        //newpos.x = newpos.x - adjust;
+        newpos.z = newpos.z - adjust;
+
+        if (animation_frame < 760)
+        {
+            transform.Rotate(-newpos * Time.deltaTime * 10);
+        }
+        else if (animation_frame < 780)
+        {
+            transform.Rotate(-newpos * Time.deltaTime * 5);
+
+        }
+        if (animation_frame > 680)
+        {
+            fade.EndScene();
+        }
+        if (animation_frame < 200)
+        {
+            adjust += 0.06f * Time.deltaTime;
+        }
+        else if (200 < animation_frame && animation_frame < 500)
+            adjust -= 0.03f * Time.deltaTime;
+        if (animation_frame == 780)
+            Application.Quit();
+
+        animation_frame++;
+	}
+	
+}
---
+++ Assets/horse_rotate.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class horse_rotate : MonoBehaviour {
+	
+	public float speed = 10f;
+	int animation_frame = 0;  
+	
+	// Joint parameters
+	const float JOINT_MIN = -35.0f;
+	const float JOINT_MAX =  35.0f;
+	float joint_rot = 0.0f;
+	int count = 0;
+	
+	void Update ()
+	{
+		// Define Variables
+		GameObject horse;
+		horse = GameObject.Find("horse");
+		Vector3 newpos = horse.transform.position;
+		newpos.y = newpos.y - 0.5f;
+		
+		// Update geometry
+		const double joint_rot_speed = 0.03;
+		double joint_rot_t = (Mathf.Sin((float)(animation_frame*joint_rot_speed)) + 1.0) / 2.0;
+		joint_rot = (float)(joint_rot_t * JOINT_MIN + (1.0 - joint_rot_t) * JOINT_MAX);
+		
+		// Rotate about a point
+		transform.RotateAround(newpos, horse.transform.right, joint_rot * Time.deltaTime);
+		
+		animation_frame++;
+	}
+}
